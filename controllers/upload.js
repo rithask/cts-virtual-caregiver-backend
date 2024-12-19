ca1a215dff98f2140f4ebb5f920966e0bc7fde20
Rithas K <rithask@icloud.com>
2024-12-19 15:51:39 +0530

    fix: pdf is send to Gemini after upload

--- controllers/upload.js
+++ controllers/upload.js
@@ -85,7 +85,72 @@ uploadRouter.post(
         $push: { pdfs: pdf._id },
       });
 
-      response.status(201).json({ pdf, message: "File uploaded successfully" });
+      // response.status(201).json({ pdf, message: "File uploaded successfully" });
+
+      const genAI = new GoogleGenerativeAI(config.GEMINI_API_KEY);
+      const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+      const latestPDF = await Pdf.findOne({ uploadedBy: request.user.id }).sort(
+        {
+          createdAt: -1,
+        }
+      );
+
+      const pdfResponse = await axios({
+        method: "GET",
+        url: latestPDF.s3Url,
+        responseType: "arraybuffer",
+      });
+
+      const pdfPart = {
+        inlineData: {
+          data: Buffer.from(pdfResponse.data).toString("base64"),
+          mimeType: "application/pdf",
+        },
+      };
+
+      const prompt = `Extract the following details from the medical document and return them in a structured JSON format:
+            {
+              "patient": {
+                "name": "<patient's name>",
+                "age": "<patient's age>"
+              },
+              "treating_consultant": {
+                "name": "<doctor's name>",
+                "specialty": "<doctor's specialty>",
+                "hospital": "<hospital name>"
+              },
+              "discharge_details": {
+                "prescribed_illness": "<diagnosed illness>",
+                "prescription": [
+                  {
+                    "medicine_name": "<medicine name>",
+                    "dosage": "<dosage instructions>",
+                    "time": "<when to take>",
+                    "duration": "<how long to take>"
+                  }
+                ],
+                "follow_up_date": "<follow-up date>",
+                "diet_recommendation": {
+                  "breakfast": ["<item 1>", "<item 2>"],
+                  "lunch": ["<item 1>", "<item 2>"],
+                  "dinner": ["<item 1>", "<item 2>"]
+                },
+                "exercise_recommendation": {
+                  "exercise_1": "<exercise details>"
+                }
+              }
+            }
+            `;
+
+      const result = await model.generateContent([prompt, pdfPart]);
+
+      const resultData = result.response.candidates[0].content.parts[0].text;
+      const resultDataJson = JSON.parse(
+        resultData.replace(/```json\n|```/g, "")
+      );
+
+      response.status(200).json({ data: resultDataJson });
     } catch (error) {
       console.error("Error uploading file:", error.message);
       response.status(500).json({ error: error.message });
